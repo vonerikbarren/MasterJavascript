cb609582658a5385102cd7ecc7e4829fa292be9e
Von'Erik Barren-Tucker <barrentucker.vonerik@gmail.com>
2020-08-24 09:41:39 -0400

    building something small for the getElementsByClassName method

---
+++ frontend/public/1-Vanilla-Javascript/1-VJS-Basics/1-JS-Output/Output1.js
@@ -0,0 +1,16 @@
+// This is Output1.js This is a playground for practicing output js functions. 
+
+// JS-1A - Output - Javascript Display Possibilities
+
+let x = 20
+let y = 5
+let z = 100
+
+let a = x + y;
+let b = y + z;
+let c = x + z;
+
+document.getElementById("idTest").innerHTML = x + y;
+document.getElementById("classTest").innerHTML = a - x;
+document.getElemenstByClassName("demo").innerHTML = a - x;
+
