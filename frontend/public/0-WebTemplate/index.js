1a9ad31f9fea378fb17a75c7bea03e2af33627b3
Von'Erik Barren-Tucker <barrentucker.vonerik@gmail.com>
2020-09-05 23:17:24 -0400

    development of grid div

--- frontend/public/0-WebTemplate/index.js
+++ frontend/public/0-WebTemplate/index.js
@@ -1,3 +1,11 @@
-var Title = document.getElementById("demo").innerHTML = "Javascript Demo Test";
+var introduction = "Welcome to: "
 
-Title = "Im Hungry";
+
+var Title = document.getElementById("demo").innerHTML = "Javascript Demo Test";
+var dateModified = document.getElementById("dateModified").innerHTML = document.lastModified;
+var documentTitle = document.getElementById("documentTitle").innerHTML = introduction + document.title;
+
+function basicAddition(num1, num2) {
+
+  return num1 + num2
+}
