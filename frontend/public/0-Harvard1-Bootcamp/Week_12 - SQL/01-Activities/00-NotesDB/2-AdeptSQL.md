3ceabdd1994b9fa180ee37ab3e093a824c410074
Von'Erik Barren-Tucker <barrentucker.vonerik@gmail.com>
2020-10-16 15:42:36 -0400

    updated notes for mySQL and added before class coding challenges from Harvard Web_Dev class

---
+++ frontend/public/0-Harvard1-Bootcamp/Week_12 - SQL/01-Activities/00-NotesDB/2-AdeptSQL.md
@@ -0,0 +1,9 @@
+# Adept (Beginner) SQL Notes
+
+``` SQL
+-- Use code below to change the password so node servers can access database. 
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root'
+
+
+```
+
---
+++ frontend/public/0-Harvard1-Bootcamp/Week_3 - Javascript/ClassJs-PreClass-Activities/wk12-c2.js
@@ -0,0 +1,21 @@
+// Prompt:
+// Write a recursive function that generates a random number
+// from 0-9 and logs it. Exit the loop if the random value === 9.
+
+function foo() {
+
+  let number = Math.floor(Math.random() * 10);
+
+  if (number === ) {
+
+    return;
+
+  } else {
+
+    console.log(number)
+    foo()
+  }
+
+}
+
+foo();
